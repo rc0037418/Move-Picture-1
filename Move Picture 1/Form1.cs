61d15fb858043c6dc6f6fc01b1e805f8935e0ae7
Swindells <rc0037418@student.riversidecollege.ac.uk>
2019-09-16 15:44:39 +0100

    Add project files.

---
+++ Move Picture 1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Move_Picture_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X + 5, pictureBox2.Location.Y);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 5);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X - 5, pictureBox2.Location.Y);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y -5);
+        }
+    }
+}
